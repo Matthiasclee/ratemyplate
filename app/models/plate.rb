9a3420bae5399e76138de8d80d632f0c3620de0a
Matthiasclee <matthias@matthiasclee.com>
2022-02-23 19:34:09 -0800

    Added more validations

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,4 +1,9 @@
 class Plate < ApplicationRecord
+  states = %w(AK AL AR AZ CA CO CT DC DE FL GA HI IA ID IL IN KS KY LA MA MD ME MI MN MO MS MT NC ND NE NH NJ NM NV NY OH OK OR PA RI SC SD TN TX UT VA VT WA WI WV WY)
+
   validates :plate, length: { maximum: 7, minimum: 1 }
   validates :plate, format: { without: /\s/, message: "can not contain spaces" }
+  validates :plate, format: { with: /[A-Z][1-9]/, message: "must only contain valid characters (A-Z 1-9)" }
+  validates :plate, uniqueness: { message: "must be unique" }
+  validates :state, inclusion: { in: states, message: "must be a valid US state" }
 end
