37a1c73efc0d9df568159276a1c89d450edd0a93
Matthiasclee <matthias@matthiasclee.com>
2022-02-27 15:20:27 -0800

    Added index to resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :plates, only: %i[ new create show ]
+  resources :plates, only: %i[ index new create show ]
   get '/plates/:id/up', to: 'plates#up', as: 'up_plate'
   root 'plates#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
