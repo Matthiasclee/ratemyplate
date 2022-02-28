540fd10eebb68b7804eb3133194b3866d4279f04
Matthiasclee <matthias@matthiasclee.com>
2022-02-27 17:14:56 -0800

    Add imageurl back

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_02_28_011051) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "score"
+    t.string "imageurl"
   end
 
 end
