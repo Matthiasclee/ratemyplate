3db42e4a50c1dcdfff5fbec6e31229dddcc99f0c
Matthiasclee <matthias@matthiasclee.com>
2022-03-12 17:06:18 -0800

    Plate shows with background

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,8 @@
 class StaticController < ApplicationController
-  def get_item
+  def get_lic_plate
     send_data(Plate.find_by(id: params[:id]).image, filename: "plate")
+  end
+  def get_blank_plate
+    send_file(Rails.root.join("public", "state_license_plates", "#{params[:state]}.jpeg"))
   end
 end
